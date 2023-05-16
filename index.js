54ed6d3b85601cd879cff3dac9a38046a6205a19
siriyusabbos <siriyusabbos@gmail.com>
2023-05-16 13:37:12 +0500

    awed
    eqw

--- index.js
+++ index.js
@@ -1052,7 +1052,7 @@ app.post("/oneproduct", async (req,res)=>{
         url: url
     })
     
-    var select="html body div.body-wrapper div#product-product.main-container div.containerSticky"
+    var select="#page-heading"
     const $ = cheerio.load(data)
     $(select).each((index, item) => {
             a = $(item).html()
