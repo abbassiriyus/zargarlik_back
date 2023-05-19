7d6ec1b4d393bec688ca3b72cf32e9d4df6d9eca
siriyusabbos <siriyusabbos@gmail.com>
2023-05-19 16:28:10 +0500

    qwe
    qwe

--- index.js
+++ index.js
@@ -837,7 +837,8 @@ app.put("/minicategory/:id", (req, res) => {
     }
 
 })
-app.get("/product/:category/:minicategory/:padcategory?number", async (req, res) => {
+app.get("/product/:category/:minicategory/:padcategory", async (req, res) => {
+    console.log(req.query.number);
     var url = `https://www.diamondsfactory.com/${req.params.category}/${req.params.minicategory}/${req.params.padcategory}?page=${req.quary.number}`
     const { data } = await axios({
         method: 'GET',
@@ -867,68 +868,8 @@ app.get("/product/:category/:minicategory/:padcategory?number", async (req, res)
 
 
 })
-app.get("/product/:category/:minicategory/:padcategory", async (req, res) => {
-    var url = `https://www.diamondsfactory.com/${req.params.category}/${req.params.minicategory}/${req.params.padcategory}`
-    const { data } = await axios({
-        method: 'GET',
-        url: url
-    })
-    var select = "div.middle-container div.categoryDiv2"
-    var a = []
-    const $ = cheerio.load(data)
-    $(select).each((index, item) => {
-        $(item).children().each((index, data) => {
-            var pushdata = JSON.parse($(data).attr().onclick.slice(15, -2))
-                        var d=$(data, "div.showbox").html()
-            var b=d.indexOf('class="sale-label">SALE</div>\n<a')
-     
-
-            var t=d.indexOf('tabindex="1" class="catLink"')
-        
-            var y=d.slice(0,b+33)+d.slice(t)
-            pushdata.code = `${y}`
-            pushdata.img = $(data).find('img').attr("src")
-            a.push(pushdata)
-        }
-        )
-    })
-    console.log(a.length);
-    res.status(200).send(a)
-
-
-})
-app.get("/product/:category/:minicategory/", async (req, res) => {
-    var url = `https://www.diamondsfactory.com/${req.params.category}/${req.params.minicategory}?page=${req.quary.number}`
-    const { data } = await axios({
-        method: 'GET',
-        url: url
-    })
-    var select = "div.middle-container div.categoryDiv2"
-    var a = []
-    const $ = cheerio.load(data)
-    $(select).each((index, item) => {
-        $(item).children().each((index, data) => {
-            var pushdata = JSON.parse($(data).attr().onclick.slice(15, -2))
-                        var d=$(data, "div.showbox").html()
-            var b=d.indexOf('class="sale-label">SALE</div>\n<a')
-     
-
-            var t=d.indexOf('tabindex="1" class="catLink"')
-        
-            var y=d.slice(0,b+33)+d.slice(t)
-            pushdata.code = `${y}`
-            pushdata.img = $(data).find('img').attr("src")
-            a.push(pushdata)
-        }
-        )
-    })
-    console.log(a.length);
-    res.status(200).send(a)
-
-
-})
 app.get("/product/:category/:minicategory", async (req, res) => {
-    var url = `https://www.diamondsfactory.com/${req.params.category}/${req.params.minicategory}`
+    var url = `https://www.diamondsfactory.com/${req.params.category}/${req.params.minicategory}?page=${req.query.number}`
     const { data } = await axios({
         method: 'GET',
         url: url
@@ -957,8 +898,68 @@ app.get("/product/:category/:minicategory", async (req, res) => {
 
 
 })
+// app.get("/product/:category/:minicategory?number", async (req, res) => {
+//     var url = `https://www.diamondsfactory.com/${req.params.category}/${req.params.minicategory}?page=${req.quary.number}`
+//     const { data } = await axios({
+//         method: 'GET',
+//         url: url
+//     })
+//     var select = "div.middle-container div.categoryDiv2"
+//     var a = []
+//     const $ = cheerio.load(data)
+//     $(select).each((index, item) => {
+//         $(item).children().each((index, data) => {
+//             var pushdata = JSON.parse($(data).attr().onclick.slice(15, -2))
+//                         var d=$(data, "div.showbox").html()
+//             var b=d.indexOf('class="sale-label">SALE</div>\n<a')
+     
+
+//             var t=d.indexOf('tabindex="1" class="catLink"')
+        
+//             var y=d.slice(0,b+33)+d.slice(t)
+//             pushdata.code = `${y}`
+//             pushdata.img = $(data).find('img').attr("src")
+//             a.push(pushdata)
+//         }
+//         )
+//     })
+//     console.log(a.length);
+//     res.status(200).send(a)
+
+
+// })
+// app.get("/product/:category", async (req, res) => {
+//     var url = `https://www.diamondsfactory.com/${req.params.category}/${req.params.minicategory}`
+//     const { data } = await axios({
+//         method: 'GET',
+//         url: url
+//     })
+//     var select = "div.middle-container div.categoryDiv2"
+//     var a = []
+//     const $ = cheerio.load(data)
+//     $(select).each((index, item) => {
+//         $(item).children().each((index, data) => {
+//             var pushdata = JSON.parse($(data).attr().onclick.slice(15, -2))
+//                         var d=$(data, "div.showbox").html()
+//             var b=d.indexOf('class="sale-label">SALE</div>\n<a')
+     
+
+//             var t=d.indexOf('tabindex="1" class="catLink"')
+        
+//             var y=d.slice(0,b+33)+d.slice(t)
+//             pushdata.code = `${y}`
+//             pushdata.img = $(data).find('img').attr("src")
+//             a.push(pushdata)
+//         }
+//         )
+//     })
+//     console.log(a.length);
+//     res.status(200).send(a)
+
+
+// })
 app.get("/product/:category", async (req, res) => {
-    var url = `https://www.diamondsfactory.com/${req.params.category}`
+    var url = `https://www.diamondsfactory.com/${req.params.category}?page=${req.query.number}`
     const { data } = await axios({
         method: 'GET',
         url: url
