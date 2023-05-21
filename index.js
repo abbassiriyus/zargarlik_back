2fa51ae4bd37ffe9968fdde62e195b9b114a4c59
siriyusabbos <siriyusabbos@gmail.com>
2023-05-21 13:05:05 +0500

    a
    a

--- index.js
+++ index.js
@@ -176,8 +176,18 @@ app.get('/page/:category', async (req, res) => {
 //     })
 app.post('/category', async (req,res)=>{
     // try{
-        console.log("test");
-        const browser = await puppeteer.launch({headless: true});
+        const browser = await puppeteer.launch({
+            args: [
+              "--disable-setuid-sandbox",
+              "--no-sandbox",
+              "--single-process",
+              "--no-zygote",
+            ],
+            executablePath:
+              process.env.NODE_ENV === "production"
+                ? process.env.PUPPETEER_EXECUTABLE_PATH
+                : puppeteer.executablePath(),
+          });
         const page = await browser.newPage();
         var page2='#page-heading'
         await page.goto(req.body.pages);
